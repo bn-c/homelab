a708e1c3b63618aaf8093a31e8efadb174faeeaf
bn-c <root.bachnc@gmail.com>
2026-04-29 12:07:22 +0000

    Add LXC configuration for Jackett

---
+++ tofu/jackett.tf
@@ -0,0 +1,31 @@
+resource "proxmox_virtual_environment_container" "jackett_ct" {
+  node_name = "pve"
+  vm_id     = 902
+  description = "Jackett container"
+  
+  initialization {
+    hostname = "jackett"
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/ubuntu-25.04-standard_25.04-1.1_amd64.tar.zst"
+    type             = "ubuntu"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr1lan"
+  }
+
+  started = true
+}
