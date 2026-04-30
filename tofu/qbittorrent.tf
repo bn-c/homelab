090d2c7f92731069db81a7eb7bb0059542f33c3a
bn-c <root.bachnc@gmail.com>
2026-04-30 02:19:16 +0000

    Add qBittorrent infrastructure and configuration

---
+++ tofu/qbittorrent.tf
@@ -0,0 +1,54 @@
+resource "proxmox_virtual_environment_container" "qbittorrent_ct" {
+  node_name    = "pve"
+  vm_id        = 903
+  description  = "qBittorrent container"
+  unprivileged = true
+
+  features {
+    nesting = true
+  }
+
+  initialization {
+    hostname = "qbittorrent"
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+    user_account {
+      password = "techisawesome"
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbpFP/1DMoIMlkxeg1W0BIfQeokpbanE61WldpqjzHe root@coder-bn-c-dev",
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+nh8Nwmib5blLo1W2rawfg4b6UKkrOwh9QF+3ARZRq tech@desk"
+      ]
+    }
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/ubuntu-25.04-standard_25.04-1.1_amd64.tar.zst"
+    type             = "ubuntu"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+  memory {
+    dedicated = 2048
+  }
+  cpu {
+    cores = 2
+  }
+
+  network_interface {
+    name     = "eth0"
+    bridge   = "vmbr1lan"
+    firewall = true
+  }
+
+  console {
+    enabled = true
+  }
+
+  started = true
+}
